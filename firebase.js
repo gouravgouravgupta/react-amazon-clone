1fc8ab634926733d58aa61b0046721721afde5bf
gouravgouravgupta <gouravgupta7879@gmail.com>
2021-07-05 09:20:10 +0530

    initial setup and some component

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyC6aXTPUs5qBWKoOBfoMjNWs2_4-05raj0",
+    authDomain: "challenge-c2885.firebaseapp.com",
+    projectId: "challenge-c2885",
+    storageBucket: "challenge-c2885.appspot.com",
+    messagingSenderId: "658215313003",
+    appId: "1:658215313003:web:6cef20764ade07c58fdb2e"
+  };
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__Container">
+                <img src="https://wgefund.org/wp-content/uploads/2016/11/AmazonSmilebanner-980x467.jpg"
+                alt="amazon banner">
+
+                </img>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
